0ec59b062ad522a41c7990c990bae0a17f29f45b
Bhekisile <bhekisilejozi@gmail.com>
2024-02-12 13:57:37 +0200

    Fetch cabin data from React Query cache

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,6 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
+import { getCabins } from "../../services/apiCabins";
+import CabinRow from "./CabinRow";
 
-const Table = styled.div`
+export const Table = styled.div`
   border: 1px solid var(--color-grey-200);
 
   font-size: 1.4rem;
@@ -9,7 +12,7 @@ const Table = styled.div`
   overflow: hidden;
 `;
 
-const TableHeader = styled.header`
+export const TableHeader = styled.header`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
   column-gap: 2.4rem;
@@ -23,3 +26,30 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+function CabinTable() {
+  const { isLoading, data: cabins, error } = useQuery({
+    queryKey: ["cabin"],
+    queryFn: getCabins,
+  });
+
+  console.log(isLoading, cabins, error);
+
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {cabins.map((cabin) => (
+        <CabinRow cabin={cabin} key={cabin.id} />
+      ))}
+    </Table>
+  )
+}
+
+export default CabinTable;
