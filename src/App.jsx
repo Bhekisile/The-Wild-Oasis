e75f2ff2381a23872eee657808c0980ee0366533
Bhekisile <bhekisilejozi@gmail.com>
2024-02-12 08:15:59 +0200

    Set up React Query

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,21 @@ import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -29,8 +40,8 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-        </BrowserRouter>
-    </>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
