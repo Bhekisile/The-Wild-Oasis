2c2f2e912e71403baaa5ef5a4a64cfafd2e92da8
Bhekisile <bhekisilejozi@gmail.com>
2024-02-15 19:11:22 +0200

    Add edit a cabin function

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -5,17 +5,25 @@ import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
 import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createCabin } from "../../services/apiCabins";
+import { createEditCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
 import FormRow from "../../ui/FormRow";
+// import PropType from 'prop-types';
 
-function CreateCabinForm() {
-  const { register, handleSubmit, reset, getValues, formState } = useForm();
+// eslint-disable-next-line react/prop-types
+function CreateCabinForm({ cabinToEdit = {} }) {
+  const { id: editId, ...editValues } = cabinToEdit;
+  const isEditSession = Boolean(editId); 
+
+  const { register, handleSubmit, reset, getValues, formState } = useForm({
+    defaultValues: isEditSession ? editValues : {},
+  });
+
   const { errors } = formState;
   const queryClient = useQueryClient();
 
-  const {mutate, isLoading: isCreating} = useMutation({
-    mutationFn: createCabin,
+  const {mutate: createCabin, isLoading: isCreating} = useMutation({
+    mutationFn: createEditCabin,
     onSuccess: () => {
       toast.success("New cabin successfully created");
       queryClient.invalidateQueries({ queryKey: ["cabins"] });
@@ -24,8 +32,23 @@ function CreateCabinForm() {
     onError: (err) => toast.error(err.message),
   });
 
+  const {mutate: editCabin, isLoading: isEditing} = useMutation({
+    mutationFn: ({ newCabinData, id}) => createEditCabin(newCabinData, id),
+    onSuccess: () => {
+      toast.success("Cabin successfully edited");
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
+      reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  const isWorking = isCreating || isEditing;
+
   function onSubmit(data) {
-    mutate({ ...data, image: data.image[0] });
+    const image = typeof data.image === "string" ? data.image : data.image[0];
+    if (isEditSession)
+    editCabin({ newCabinData: {...data, image }, id: editId });
+    else createCabin({ ...data, image: image });
     // return true;
   }
 
@@ -72,7 +95,7 @@ function CreateCabinForm() {
 
       <FormRow label="Cabin photo" error = {errors?.image?.message}>
         <FileInput id="image" accept="image/*" {...register("image", {
-          required: "This field is required",
+          required: isEditSession ? false : "This field is required",
         })} />
       </FormRow>
 
@@ -81,10 +104,14 @@ function CreateCabinForm() {
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button disabled={isCreating}>Add cabin</Button>
+        <Button disabled={isWorking}>{isEditSession ? "Edit cabin" : "Create new cabin" }</Button>
       </FormRow>
     </Form>
   );
 }
+
+// CreateCabinForm.propType = {
+//   cabinToEdit: PropType.object.isRequired,
+// }
 
 export default CreateCabinForm;
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -14,15 +14,26 @@ export async function getCabins() {
   return data;
 }
 
-export async function createCabin(newCabin) {
+export async function createEditCabin(newCabin, id) {
+  const hasImagePath = newCabin.image?.startsWith?.(SUPABASE_URL);
+
   const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll("/", "");
 
-  const imagePath = `${SUPABASE_URL}/storage/v1/object/public/cabin-images/${imageName}`;
+  const imagePath = hasImagePath ? newCabin.image : `${SUPABASE_URL}/storage/v1/object/public/cabin-images/${imageName}`;
 
-  // create cabin
-  const { data, error } = await supabase
-  .from('cabins')
-  .insert([{ ...newCabin, image: imagePath }])
+  // create/edit cabin
+  let query = supabase.from("cabins");
+
+  // A) create
+  if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
+
+  // B) edit
+  // eslint-disable-next-line no-unused-vars
+  if (id) query = query.update({ ...newCabin, image: imagePath })
+  .eq('id', id);
+  
+
+  const { data, error } = await query.select().single();
 
   if (error) {
     console.error(error);
