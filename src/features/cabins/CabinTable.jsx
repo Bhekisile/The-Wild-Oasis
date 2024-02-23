a75244d449f14474b6aa67793a0a33c5be303e9b
Bhekisile <bhekisilejozi@gmail.com>
2024-02-23 16:37:43 +0200

    Filter cabins by discount

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,47 +1,24 @@
-import styled from "styled-components";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Spinner from '../../ui/Spinner';
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-// import { useSearchParams } from "react-router-dom";
-
-// export const Table = styled.div`
-//   border: 1px solid var(--color-grey-200);
-
-//   font-size: 1.4rem;
-//   background-color: var(--color-grey-0);
-//   border-radius: 7px;
-//   overflow: hidden;
-// `;
-
-export const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
-  // const [searchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   
   if (isLoading) return <Spinner />;
 
-  // const filterValue = searchParams.get("discount") || "all";
-  // let filteredCabins;
-  // if (filterValue === "all") filteredCabins = cabins;
-  // if (filterValue === "no-discount") filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
-  // if (filterValue === "with-discount") filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+  const filterValue = searchParams.get("discount") || "all";
+  let filteredCabins;
 
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount") filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount") filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  console.log(filteredCabins);
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,5 +1,6 @@
 import { useSearchParams } from "react-router-dom";
 import styled, { css } from "styled-components";
+import PropTypes from "prop-types";
 
 const StyledFilter = styled.div`
   border: 1px solid var(--color-grey-100);
@@ -35,21 +36,28 @@ const FilterButton = styled.button`
   }
 `;
 
-function Filter() {
+function Filter({ filterField, options }) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get(filterField) || options.at(0).value
 
   function handleClick(value) {
-    searchParams.set("discount", value);
+    searchParams.set(filterField, value);
     setSearchParams(searchParams);
   }
 
   return (
     <StyledFilter>
-      <FilterButton onClick={() => handleClick("all")}>All</FilterButton>
-      <FilterButton onClick={() => handleClick("no-discount")}>No discount</FilterButton>
-      <FilterButton onClick={() => handleClick("with-discount")}>With discount</FilterButton>
-    </StyledFilter>
+      {options.map((option) => (
+        <FilterButton key={option.value} onClick={() => handleClick(option.value)} active={option.value === currentFilter} >{option.label}</FilterButton>
+      ))}
+      </StyledFilter>
+      
   );
+}
+
+Filter.propTypes = {
+  filterField: PropTypes.string.isRequired,
+  options: PropTypes.any.isRequired
 }
 
 export default Filter;
